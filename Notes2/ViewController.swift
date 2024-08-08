d8d884fd8c3c33a7c20fe9d71810334390bd7e8e
jozek-hajduk_globant <jozek.hajduk@globant.com>
2024-08-08 16:45:36 -0500

    Initial Commit

---
+++ Notes2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes2
+//
+//  Created by Jozek Andrzej Hajduk Sanchez on 8/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
